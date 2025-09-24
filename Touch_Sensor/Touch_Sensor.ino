11f168723a89d46272b88eb68b5288b647a4ca0d
Aslı Kurt <aslikurt79@hotmail.com>
2025-09-24 13:30:40 +0300

    Update

--- Touch_Sensor/Touch_Sensor.ino
+++ Touch_Sensor/Touch_Sensor.ino
@@ -1,5 +1,5 @@
-
 const int TouchPin = 2;                                     // Arduino - 2nd PIN --> Touch Sensor - OUT Pin
+const int LedPin = 3;
 
 int PreviousTouchState,TouchState  = 0;                     // Touch state declarated + initializated to 0
 
@@ -8,20 +8,26 @@ int counter = 0;                                            // Number of touches
 void setup() {
   Serial.begin(9600);                                       // For Serial Screen
   pinMode(TouchPin, INPUT);                                 // Touch Sensor's OUT --> Arduino's IN
+  pinMode(LedPin, OUTPUT);
 }
 
 void loop() {
-  // Sensörden gelen değeri oku
+
   TouchState = digitalRead(TouchPin);                       // Read the digital data 
 
   if (PreviousTouchState == LOW & TouchState == HIGH) {     // The touch is detected
-    // Dokunma algılandı
 
     counter++;                                              // Count number of touch
     Serial.print("Total : ");
     Serial.println(counter);   
+
+    digitalWrite(LedPin, HIGH);                             // Turn on the led
+
   } 
   
+  if (TouchState == LOW)                                    // No touch
+    digitalWrite(LedPin, LOW);                              // Turn off the led
+
   PreviousTouchState = TouchState;                          // Update for new loop
 
 }
